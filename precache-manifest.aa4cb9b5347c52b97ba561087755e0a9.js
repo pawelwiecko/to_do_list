97a979db28cb06fce75a52eaf6bb0f3bf0e057a9
Pawel Wiecko <pawelwiecko2018@gmail.com>
2020-05-07 16:00:09 +0200

    Updates

---
+++ precache-manifest.aa4cb9b5347c52b97ba561087755e0a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18bb48590e77e03941c4",
+    "url": "/to_do_list/static/js/runtime~main.015986e0.js"
+  },
+  {
+    "revision": "d5871e4a03c608f9313a",
+    "url": "/to_do_list/static/js/main.7d01a12e.chunk.js"
+  },
+  {
+    "revision": "e84cceccc2841b85d993",
+    "url": "/to_do_list/static/js/2.f4426b14.chunk.js"
+  },
+  {
+    "revision": "d5871e4a03c608f9313a",
+    "url": "/to_do_list/static/css/main.270e99eb.chunk.css"
+  },
+  {
+    "revision": "cdd4ab088d358d525099aa991fc56f89",
+    "url": "/to_do_list/index.html"
+  }
+];
